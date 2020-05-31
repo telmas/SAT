4fe24a417e621027006c847c8bd36814aa9ce902
Tedi <tedielmas@outlook.com>
2020-05-31 13:58:37 +0200

    Print Assignment in Binary

--- src/alg/sat/entity/Assignment.java
+++ src/alg/sat/entity/Assignment.java
@@ -1,21 +1,22 @@
 package alg.sat.entity;
 
-import java.util.BitSet;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Assignment {
     private Map<Integer, Boolean> solution;
+    private FixedSizeBitSet fixedSizeBitSet;
 
     public Assignment() {
         this.setSolution(new HashMap<>());
+        this.setFixedSizeBitSet(new FixedSizeBitSet(solution.size()));
     }
 
     public Assignment(Formula formula, BitSet bitSet) {
         HashMap<Integer, Boolean> map = new HashMap<>();
         List<Integer> literalIndexes = formula.getAllLiteralIndexes();
         literalIndexes.forEach(index -> map.putIfAbsent(index, bitSet.get(index - 1)));
+        this.setFixedSizeBitSet(new FixedSizeBitSet(map.size()));
         this.setSolution(map);
     }
 
@@ -29,6 +30,39 @@ public class Assignment {
 
     @Override
     public String toString() {
-        return "Assignment{" + "solution=" + solution + '}';
+        FixedSizeBitSet fixedSizeBitSet = new FixedSizeBitSet(solution.size());
+        solution.forEach((key, value) -> {
+            if (value) {
+                fixedSizeBitSet.set(key - 1);
+            }
+        });
+        String s = "Assignment" + solution;
+        String b = "Assignment{" + "Binary:" + fixedSizeBitSet + '}';
+        return s + "\n" + b;
+    }
+
+    public FixedSizeBitSet getFixedSizeBitSet() {
+        return fixedSizeBitSet;
+    }
+
+    public void setFixedSizeBitSet(FixedSizeBitSet fixedSizeBitSet) {
+        this.fixedSizeBitSet = fixedSizeBitSet;
+    }
+
+
+    private static class FixedSizeBitSet extends BitSet {
+        private final int nbits;
+
+        public FixedSizeBitSet(final int nbits) {
+            super(nbits);
+            this.nbits = nbits;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder buffer = new StringBuilder(nbits);
+            IntStream.range(0, nbits).mapToObj(i -> get(i) ? '1' : '0').forEach(obj -> buffer.append(obj).append(", "));
+            return buffer.toString().replaceAll(", $", "");
+        }
     }
 }
